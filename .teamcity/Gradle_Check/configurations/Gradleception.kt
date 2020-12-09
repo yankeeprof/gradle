bf9ab205250a78e2d584f2da2433c34065ffd0fb
Bo Zhang <bo@gradle.com>
2020-12-09 23:17:00 +0800

    Move distribution integ test to first run

--- .teamcity/Gradle_Check/configurations/Gradleception.kt
+++ .teamcity/Gradle_Check/configurations/Gradleception.kt
@@ -33,13 +33,13 @@ class Gradleception(model: CIBuildModel, stage: Stage) : BaseGradleBuildType(mod
     applyDefaults(model, this, ":distributions-full:install", notQuick = true, extraParameters = "-Pgradle_installPath=dogfood-first $buildScanTagForType", extraSteps = {
         localGradle {
             name = "BUILD_WITH_BUILT_GRADLE"
-            tasks = "clean :distributions-full:install"
+            tasks = "clean :distributions-full:install :distributions-integ-tests:forkingIntegTest"
             gradleHome = "%teamcity.build.checkoutDir%/dogfood-first"
             gradleParams = "-Pgradle_installPath=dogfood-second -PignoreIncomingBuildReceipt=true $defaultParameters"
         }
         localGradle {
             name = "QUICKCHECK_WITH_GRADLE_BUILT_BY_GRADLE"
-            tasks = "clean sanityCheck test distributionsIntegTests:forkingIntegTest"
+            tasks = "clean sanityCheck test"
             gradleHome = "%teamcity.build.checkoutDir%/dogfood-second"
             gradleParams = defaultParameters
         }
